5c2b13bc42ad8cf7cbfad5526d423afd9e86e816
Bikal <bikaladhikari5@gmail.com>
2024-04-11 16:25:58 +1000

    Css added

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Author = () => {
-  return <h4>Jordan Moore </h4>;
+  return (
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+      Jordan Moore
+    </h4>
+  );
 };
 
 export default Author;
